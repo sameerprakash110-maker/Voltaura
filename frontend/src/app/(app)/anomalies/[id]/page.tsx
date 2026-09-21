eb72617ce3116a8a830b2c10b4377a88ba3d3875
SREEJANI BHATTACHARAYA <bhattacharyasreejani@gmail.com>
2026-09-22 00:19:13 +0530

    Anomaly ooage fixed

--- frontend/src/app/(app)/anomalies/[id]/page.tsx
+++ frontend/src/app/(app)/anomalies/[id]/page.tsx
@@ -21,6 +21,7 @@ import { useParams, useSearchParams } from "next/navigation";
 import * as React from "react";
 
 import { EvidenceList } from "@/components/cards/domain-cards";
+import { InvestigationPanel } from "@/components/investigation/InvestigationPanel";
 import {
   ActualVsExpectedChart,
   BeforeAfterChart,
@@ -390,6 +391,9 @@ export default function AnomalyDetailPage() {
           </div>
         </Panel>
       </section>
+
+      {/* ---- deterministic investigation ---- */}
+      <InvestigationPanel anomalyId={anomaly.id} />
 
       {/* ---- recommendation ---- */}
       {recommendation ? (
